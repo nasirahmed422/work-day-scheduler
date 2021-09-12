ba262212243cb68bf01803bf55fddf22e0170ab8
Nasir Ahmed <nahmed@utilant.com>
2021-09-12 18:00:44 -0400

    added local storage and retrieval functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,11 @@
 //This will get the current date and append it to the element with currentDay ID. 
 var currentDay = moment();
-var displayDate = moment(currentDay).format("dddd MMMM Do");
+var displayDate = moment(currentDay).format("dddd MMMM Do, YYYY");
 $("#currentDay").text(displayDate);
-
-//These identify the text boxes and will help change the color.
-var nineAM = document.querySelector('#nineAM');
-var tenAM = document.querySelector("#tenAM");
-var elevenAM = document.querySelector("#elevenAM");
-var twelvePM = document.querySelector("#twelvePM");
-var onePM = document.querySelector("#onePM");
-var twoPM = document.querySelector("#twoPM");
-var threePM = document.querySelector("#threePM");
-var fourPM = document.querySelector("#fourPM");
-var fivePM = document.querySelector("#fivePM");
 
 //This is the function that will change colors based on past, present, and future. 
 function textBlockColor(timeBoxElement, bTime, aTime) {
     var format = 'hh:mm:ss'
-
-    // var time = moment() gives you current time. no format required.
     var currentTime = moment();
     var beforeTime = moment(bTime, format);
     var afterTime = moment(aTime, format);
@@ -32,9 +19,10 @@ function textBlockColor(timeBoxElement, bTime, aTime) {
     else if (currentTime.isAfter(afterTime)) {
         $(timeBoxElement).addClass("past");
         //Added this line because you shouldnt schedule something in the past.
-        $(timeBoxElement).attr('disabled', '');
+        //$(timeBoxElement).attr('disabled', '');
     }
 }
+
 //The following will run to change the box colors. 
 //I would've put this in a loop but there aren't that many.
 textBlockColor(nineAM, '09:00:00', '10:00:00');
@@ -46,3 +34,22 @@ textBlockColor(twoPM, '14:00:00', '15:00:00');
 textBlockColor(threePM, '15:00:00', '16:00:00');
 textBlockColor(fourPM, '16:00:00', '17:00:00');
 textBlockColor(fivePM, '17:00:00', '18:00:00');
+
+//This array will be looped through for local storage and will use the ID on the page as its local storage key.
+var inputs = ['nineAM', 'tenAM', 'elevenAM', 'twelvePM', 'onePM', 'twoPM', 'threePM', 'fourPM', 'fivePM']
+
+for (var i = 0; i < inputs.length; i++) {
+    var slot = localStorage.getItem(inputs[i]);
+    $("#" + inputs[i]).val(slot);
+}
+
+//This function stores values in local which can be accessed later.
+//I am using the ID as the as the key
+$(".saveBtn").click(function (event) {
+    event.preventDefault();
+
+    var addedNote = $(this).siblings(".description").val();
+    var listItem = $(this).siblings("textarea").attr("id");
+    localStorage.setItem(listItem, addedNote);
+
+});
